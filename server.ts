ffe7ef8db3057506afda33863619b6d5337e082f
Temistocles Zwang <temis2st@gmail.com>
2023-03-10 14:39:13 -0300

    ref: interface

--- server.ts
+++ server.ts
@@ -1,7 +1,28 @@
 import * as net from 'net';
 
+interface Cliente {
+   chances: number;
+   palavraAdvinhar: string;
+}
+
 function escreverNoSocket(socket: net.Socket, mensagem: string): void {
    socket.write(`${mensagem}\n`);
+}
+
+function verificarLetra(letra: string, cliente: Cliente): string {
+   if (cliente.palavraAdvinhar.includes(letra)) {
+      return `A letra ${letra} está presente na palavra`;
+   } else {
+      cliente.chances--;
+      return `A letra ${letra} não está presente na palavra`;
+   }
+}
+
+function gerarPlacarIndividual(cliente: Cliente): { chances: number; palavraAdvinhar: string } {
+   return {
+      chances: cliente.chances,
+      palavraAdvinhar: cliente.palavraAdvinhar,
+   };
 }
 
 
@@ -11,38 +32,24 @@ export function meuServer(): void {
       console.log(`Cliente conectado: ${remoteAddress}:${remotePort}`);
       escreverNoSocket(socket, 'Bem vindo');
 
-      let chances = 8;
-      const palavraAdvinhar = 'teste';
+      const cliente: Cliente = {
+         chances: 8,
+         palavraAdvinhar: 'teste',
+      };
 
-      function verificarLetra(letra: string): string {
-         if (palavraAdvinhar.includes(letra)) {
-            return `A letra ${letra} está presente na palavra`;
-         } else {
-            chances--;
-            return `A letra ${letra} não está presente na palavra`;
-         }
-      }
-
-      function gerarPlacarIndividual(): { chances: number; palavraAdvinhar: string } {
-         return {
-            chances,
-            palavraAdvinhar,
-         };
-      }
-
-      escreverNoSocket(socket, `Você tem ${chances} chances iniciais. Digite uma letra:`);
+      escreverNoSocket(socket, `Você tem ${cliente.chances} chances iniciais. Digite uma letra:`);
 
       socket.on('data', (data: Buffer) => {
          const msgDoCliente = data.toString().trim();
-
+//. Essa parte vai precisar ser refatorada
          if (msgDoCliente === 'desconectar') {
             escreverNoSocket(socket, 'Desconectado');
             socket.end();
             console.log(`Cliente desconectado: ${remoteAddress}:${remotePort}`);
          } else if (msgDoCliente.length === 1) {
-            const resultado = verificarLetra(msgDoCliente);
+            const resultado = verificarLetra(msgDoCliente, cliente);
             escreverNoSocket(socket, `RESULT:${resultado}`);
-            const placarIndividual = gerarPlacarIndividual();
+            const placarIndividual = gerarPlacarIndividual(cliente);
             escreverNoSocket(socket, `CHANCES:${placarIndividual.chances}`);
             // escreverNoSocket(socket, `PALAVRA:${placarIndividual.palavraAdvinhar}`);
          }
